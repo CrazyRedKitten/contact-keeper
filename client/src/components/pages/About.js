fe3dd7195e724bbb04b92e3198a2d9e985cc4890
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-01-14 20:41:54 +0200

    Created Home and About pages and added routing between them

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div className='container'>
+      <h1>About This App</h1>
+      <p className='my-1'>This is full stack react app for keeping contacts</p>
+      <p className='bg-dark p'> Version 1.0.0</p>
+    </div>
+  );
+};
