9e6965b09f58ec19b628091adfb06943014387c3
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-01-19 03:50:12 +0200

    Add contact to Database

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -16,13 +16,13 @@ export const Contacts = () => {
       <TransitionGroup>
         {filtered !== null
           ? filtered.map(contact => (
-              <CSSTransition key={contact.id} timeout={500} classNames='item'>
-                <ContactItem key={contact.id} contact={contact} />
+              <CSSTransition key={contact._id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
               </CSSTransition>
             ))
           : contacts.map(contact => (
-              <CSSTransition key={contact.id} timeout={500} classNames='item'>
-                <ContactItem key={contact.id} contact={contact} />
+              <CSSTransition key={contact._id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
               </CSSTransition>
             ))}
       </TransitionGroup>
