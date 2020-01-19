81295240e0f2a055d61518c1ab4a99bc8f2481e7
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-01-19 04:46:56 +0200

    Added update contact in Database

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -58,6 +58,24 @@ const ContactState = props => {
     }
   };
 
+  //Update a contact
+  const updateContact = async contact => {
+    const config = {
+      headers: { "Content-Type": "application/json" }
+    };
+
+    try {
+      const response = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: response.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.error });
+    }
+  };
+
   //Clear contacts
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -71,11 +89,6 @@ const ContactState = props => {
   //Clear Current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  //Update a contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   //Filter contacts
